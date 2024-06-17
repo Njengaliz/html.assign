73fc5591da51de2d45689426d102e77a75bf089b
Njengaliz <elizabethnjenga8116@gmail.com>
2024-06-17 17:43:39 +0300

    firebase

---
+++ app.js
@@ -0,0 +1,50 @@
+//initialize firebase with config
+firebase.initializeapp({
+    apiKey: "AIzaSyBNfvqG5r456yGUJ1zYolHqwBGRIIwr3XY",
+  authDomain: "plp-web-dev.firebaseapp.com",
+  projectId: "plp-web-dev",
+  storageBucket: "plp-web-dev.appspot.com",
+  messagingSenderId: "702356608082",
+  appId: "1:702356608082:web:66f45dd0905a3185f2bb8b"
+});
+
+const db = firebase.firestore();
+// Function to add a task
+function addTask() {
+    const taskInput = document.getElementById("task-input");
+    const task = taskInput.value.trim();
+    if (task !== "") {
+        db.collection("tasks").add({
+            task: task,
+            timestamp: firebase.firestore. FieldValue.serverTimestamp(),
+        });
+        taskInput.value = "";
+    }
+}
+// Function to render tasks
+function renderTasks(doc) {
+    const taskList = document.getElementById("task-list");
+    const taskItem = document.createElement("li");
+    taskItem.className = "task-item"
+    taskItem.innerHTML = `
+    <span>${doc.data().task}</span>
+    <button onclick="deleteTask('${doc.id}')">Delete</button>
+    `;
+    taskList.appendChild(taskItem);
+}
+// Real-time listener for tasks
+db.collection("tasks")
+.orderBy("timestamp", "desc")
+.onSnapshot(snapshot => {
+    const changes = snapshot.docChanges();
+    changes.forEach(change => {
+        if (change.type === "added") {
+            renderTasks(change.doc);
+        }
+    });
+});
+// Function to delete a task
+function deleteTask(id) {
+    db.collection("tasks").doc(id).delete();
+    location.reload();
+}
